7e911378683a1c8e920389dfbe53b4e453d80e9e
Lobo Torres <lobo@quiltro.org>
2025-11-07 12:14:33 -0300

    update README

--- README.md
+++ README.md
@@ -1,32 +1,20 @@
-# gris
+# Gris
 
-catlang experiments (warning: awful code)
+These are some statically typed concatenative experiments.
 
-## iron
+**Iron** is the current (public) interpreter draft.
+Executing the interpreter can be done like so:
 
-`iron` is the current interpreter draft
+```
+$ dune exec -- ./iron/main.exe < ./iron/examples/addmul.iron
+User definitions:
+  ! : [Int] -> []
+  println : [Str] -> []
+  addmul : [Int Int Int] -> [Int]
+  scream : [Str] -> [Str]
+9!
+```
 
-    $ dune exec -- ./iron/main.exe 'def over (swap dup bury); def nip (swap drop); 3 4 over + *'
-    Inferred effect: [] -> [Int]
-    User definitions:
-        nip : [a b] -> [b]
-        over : [a b] -> [a b a]
-    Resulting stack: [21]
-
-the type checker is still a work in progress, as it needs to be reworked to use
-the concept of the latent stack as the interpreter does, so programs that would
-execute correctly will have their effect incorrectly inferred.
-
-i am working on getting this done, i just need to figure out how :-)
-
-### to-do
-
-- [ ] finish the type checker
-- [x] rework lexer and parser... perhaps?
-  - [ ] done, but still needs some work, especially since compiling the parser
-    throws some warnings (baby's first parser yay \^_\^)
-
-    ```
-    Warning: one state has reduce/reduce conflicts.
-    Warning: one reduce/reduce conflict was arbitrarily resolved.
-    ```
+There's no row polymorphism in the type system yet, so inferring the types of
+block literals (quotations) doesn't work yet (as of now it simply throws an
+exception.)
--- iron/Main.ml
+++ iron/Main.ml
@@ -2,40 +2,32 @@ open Iron
 open Printf
 
 let () =
-  if Array.length Sys.argv < 2 then
-    Printf.eprintf "usage: %s program...\n" Sys.argv.(0)
-  else
-    let code =
-      String.concat " " (Array.to_seq Sys.argv |> Seq.drop 1 |> List.of_seq)
-    in
-    let lexer = Lexing.from_string code in
-    let expr = Parser.program Lexer.token lexer in
-    let tenv = Infer.make_ty_env () in
-    let eff = Infer.infer tenv expr in
+  let lexer = Lexing.from_channel stdin in
+  let expr = Parser.program Lexer.token lexer in
+  let tenv = Infer.make_ty_env () in
+  let eff = Infer.infer tenv expr in
 
-    (* Print inferred effects *)
-    (match eff with
-    | [], [] -> ()
-    | _, _ ->
-        eprintf "Inferred effect: %s\n" (Typing.string_of_eff eff));
+  (* Print inferred effects *)
+  (match eff with
+  | [], [] -> ()
+  | _, _ -> eprintf "Inferred effect: %s\n" (Typing.string_of_eff eff));
 
-    (* Print user definitions with their inferred effects *)
-    if Hashtbl.length tenv.sigs <> 0 then (
-      prerr_endline "User definitions:";
-      Hashtbl.iter
-        (fun name eff ->
-          eprintf "  %s : %s\n" name (Typing.string_of_eff eff))
-        tenv.sigs);
+  (* Print user definitions with their inferred effects *)
+  if Hashtbl.length tenv.sigs <> 0 then (
+    prerr_endline "User definitions:";
+    Hashtbl.iter
+      (fun name eff -> eprintf "  %s : %s\n" name (Typing.string_of_eff eff))
+      tenv.sigs);
 
-    (* Execute the code *)
-    let env = Interpreter.make_env tenv in
-    Out_channel.flush stderr;
-    try
-      Interpreter.interpret env expr;
-      Out_channel.flush_all ();
-      if Stack.length env.data > 0 then
-        eprintf "Resulting stack: [%s]\n"
-          (Stack.to_seq env.data |> List.of_seq
-          |> List.rev_map Interpreter.string_of_value
-          |> String.concat " ")
-    with Interpreter.Dirty -> ()
+  (* Execute the code *)
+  let env = Interpreter.make_env tenv in
+  Out_channel.flush stderr;
+  try
+    Interpreter.interpret env expr;
+    Out_channel.flush_all ();
+    if Stack.length env.data > 0 then
+      eprintf "Resulting stack: [%s]\n"
+        (Stack.to_seq env.data |> List.of_seq
+        |> List.rev_map Interpreter.string_of_value
+        |> String.concat " ")
+  with Interpreter.Dirty -> ()
